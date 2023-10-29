ef996df91d37cf2c06952c1acf18298b900b5b8c
Mehedimid <mehedihasan25276@gmail.com>
2023-10-29 19:30:50 +0600

    server is running successfully

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000
+
+// =======middlewares======
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.send('hello server asflkjf')
+})
+
+app.listen(port, () => {
+    console.log(`running port on: ${port} ` )
+})
