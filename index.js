a7a89ff38fba01540a23c177e3784d8289d87b6d
Mehedimid <mehedihasan25276@gmail.com>
2023-10-31 19:20:20 +0600

    vercel api create and error fixed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -8,12 +9,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// mehedi117
-// rjTMT9nLhkCta1Tg
 // ================================================================
 
+
+
 const uri =
-  "mongodb+srv://mehedi117:rjTMT9nLhkCta1Tg@cluster0.virnuu4.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_MUSER}:${process.env.DB_MPASS}@cluster0.virnuu4.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productCollection = client.db("productDB").collection("products");
     const cartCollection = client.db("productDB").collection("cart");
@@ -40,12 +42,16 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/products/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await productCollection.findOne(query);
-      res.send(result);
-    });
+    app.get('/brandproducts/:brand' , async (req, res) => {
+      const brand = req.params.brand;
+      const query = {brand : (brand)}
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray(query);
+      res.send(result)
+    })
+
+
+   
 
     app.put("/products/:id" , async(req, res)=>{
       const id = req.params.id
@@ -69,7 +75,6 @@ async function run() {
 
     app.post("/products", async (req, res) => {
       const newProduct = req.body;
-      console.log(newProduct);
       const result = await productCollection.insertOne(newProduct);
       res.send(result);
     });
@@ -81,8 +86,9 @@ async function run() {
     res.send(result);
   });
 
-  app.post('/cart/', async(req, res)=>{
+  app.post('/cart', async(req, res)=>{
     const card = req.body;
+    console.log(card)
     const result = await cartCollection.insertOne(card);
     res.send(result)
   })
@@ -94,7 +100,14 @@ async function run() {
     res.send(result)
   })
 
-
+  app.get("/products/:id",  async (req, res)=> {
+    const id = req.params.id
+    // console.log(id)
+    const query = {_id: new ObjectId(id)}
+    const result = await productCollection.findOne(query)
+    res.send(result)
+    
+  })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
