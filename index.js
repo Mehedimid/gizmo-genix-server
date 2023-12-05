480b8964bbde2916d16a272003c19cfb8912536d
Mehedimid <mehedihasan25276@gmail.com>
2023-12-05 20:42:25 +0600

    environment varaible has done

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 const uri =
-  `mongodb+srv://mehedi117:Mha1381mHa@cluster0.virnuu4.mongodb.net/?retryWrites=true&w=majority`;
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.virnuu4.mongodb.net/?retryWrites=true&w=majority`;
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
